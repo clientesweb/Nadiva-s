69a4d6833ee06ef42c56f5bdf2d1ec870e1ffa8d
clientesweb <alexisifran32@gmail.com>
2024-12-26 18:47:18 -0300

    Actualizar image-loader.ts

--- app/lib/image-loader.ts
+++ app/lib/image-loader.ts
@@ -1,7 +1,7 @@
 import type { ImageLoader } from 'next/image'
 
 const imageLoader: ImageLoader = ({ src, width, quality = 75 }) => {
-  return `${src}?w=${width}&q=${quality}`
+  return `${src}${src.includes('?') ? '&' : '?'}w=${width}&q=${quality}`
 }
 
 export default imageLoader
