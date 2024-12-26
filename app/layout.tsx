d29b748fa2983cb59fe4387385cc143187051054
clientesweb <alexisifran32@gmail.com>
2024-12-26 19:07:37 -0300

    Actualizar layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,9 +27,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" suppressHydrationWarning>
-      <head>
-        <link rel="stylesheet" href="/_next/static/css/app/layout.css" />
-      </head>
       <body className={`${cormorant.variable} ${montserrat.variable} font-sans`}>
         {children}
       </body>
