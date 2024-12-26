3b41fe61c379099d9c7b9c04b8d90007dcaf7d16
clientesweb <alexisifran32@gmail.com>
2024-12-26 17:35:07 -0300

    Actualizar payment-methods.tsx

--- components/payment-methods.tsx
+++ components/payment-methods.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "@/app/components/ui/card"
 import { CreditCard, Banknote, ArrowRightLeft, ShoppingCart } from 'lucide-react'
 
 const methods = [
